413f0ad68a9744864bd56692638b36311a28dacc
Nikita Shtokolov <shtokolov2003@mail.ru>
2024-08-25 14:00:50 +0300

    worked server

--- server.go
+++ server.go
@@ -1,7 +1,59 @@
 package main
 
-import "fmt"
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+)
+
+type Person struct {
+	Name string
+	Age  int
+}
+
+var people []Person
 
 func main() {
-	fmt.Print(1)
+	http.HandleFunc("/people", peopleHandler)
+	http.HandleFunc("/health", healthCheckHandler)
+	log.Println("server start listening on port 8080")
+	err := http.ListenAndServe("localhost:8080", nil)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+}
+
+func peopleHandler(w http.ResponseWriter, r *http.Request) {
+	switch r.Method {
+	case http.MethodGet:
+		getPeople(w, r)
+	case http.MethodPost:
+		postPerson(w, r)
+	default:
+		http.Error(w, "Invalid http method ", http.StatusMethodNotAllowed)
+	}
+}
+
+func getPeople(w http.ResponseWriter, r *http.Request) {
+	json.NewEncoder(w).Encode(people)
+	fmt.Fprintf(w, "Get people: `%v`", people)
+}
+
+func postPerson(w http.ResponseWriter, r *http.Request) {
+	var person Person
+	err := json.NewDecoder(r.Body).Decode(&person)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	people = append(people, person)
+	fmt.Fprintf(w, "post new person: `%v`", person)
+
+}
+
+func healthCheckHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "http web-server works correctly")
 }
